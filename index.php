e4034565797ef5e8a4b9040eb83a5247f0a668fb
khoursaaf <khoursa@gmail.com>
2018-11-19 10:00:50 +0100

    fist commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Exercise</title>
+    </head>
+    <body>
+        <h1>Créer trois variables lastname , firstname et age et les initialiser avec les valeurs de votre choix. Attention age est de type entier. Afficher leur contenu.</h1>
+        <?php
+      $lastname = 'Adoum Fadoul';
+      $firstname = 'Khoursa';
+      $age = 21;
+      echo 'Je m\'appelle ',$lastname,' ',$firstname,' et j\'ai ',$age,' ans.';
+       ?>
+    </body>
+</html>
