9f3dcc28de3ea490872a9c7d786166eeceb3da72
khoursaaf <khoursa@gmail.com>
2018-11-20 16:18:38 +0100

    correction

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Exercise</title>
-    </head>
-    <body>
-        <h1>Créer trois variables lastname , firstname et age et les initialiser avec les valeurs de votre choix. Attention age est de type entier. Afficher leur contenu.</h1>
-        <?php
-      $lastname = 'Adoum Fadoul';
-      $firstname = 'Khoursa';
-      $age = 21;
-      echo 'Je m\'appelle ',$lastname,' ',$firstname,' et j\'ai ',$age,' ans.';
-       ?>
-    </body>
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercise</title>
+  </head>
+  <body>
+    <p>Créer trois variables lastname , firstname et age et les initialiser avec les valeurs de votre choix. Attention age est de type entier. Afficher leur contenu.</p>
+    <?php
+    $lastname = 'Adoum Fadoul';
+    $firstname = 'Khoursa';
+    $age = 21;
+    ?>
+    <p><?phpecho 'Je m\'appelle ',$lastname,' ',$firstname,' et j\'ai ',$age,' ans.';?>
+    </p>
+  </body>
 </html>
